20cad078e0eac3ddf2de1f8b6123fe0a06dc0513
Blaxu <aghinpg@gmail.com>
2018-04-24 22:30:51 +0200

    nie wiem jak tam twoja

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, const char * argv[]) {
     printf("Hello, World!\n");
-    printf("Hello, World!\n");
+    printf("ja nie wiem jak tam twoja liga akademicka !\n");
     printf("Hello, World!\n");
     printf("Hello, World!\n");
     printf("Hello, World!\n");
