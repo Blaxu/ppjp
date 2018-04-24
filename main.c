70febae46b90ec0ac04426c54cfb3a5904970e0e
Piotr Przygoda <piotrprzygoda@MacBook-Air-Piotr.local>
2018-04-24 22:34:14 +0200

    Poprawa

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-<<<<<<< HEAD
     printf("Hello, World!\n");
-    printf("ja nie wiem jak tam twoja liga akademicka !\n");
-=======
+    printf("ja nie wiem jak tam moja liga akademicka !\n");
     printf("Hello, Świat!\n");
     printf("Hello, World!\n");
->>>>>>> ace697007ec6034c2722ad9de13f9c16a0b72f4c
     printf("Hello, World!\n");
     printf("Hello, World!\n");
     printf("Hello, World!\n");
